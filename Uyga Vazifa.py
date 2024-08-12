ed9e5a402220f5653092cc0aad2a3f1bc7cd1b19
dilmurod1301 <dilmurod.1301@gmail.com>
2024-08-12 22:59:27 +0500

    Create Uyga Vazifa.py

---
+++ Uyga Vazifa.py
@@ -0,0 +1,25 @@
+import math
+
+x=144
+print(math.sqrt(x))
+
+
+
+print(pow(2,10))
+
+
+import random
+son = random.randint(55,155)
+print(son)
+
+colors = ["qizil", "yashil", "ko'k", "sariq", "qora"]
+color = random.choice(colors)
+print(color)
+
+
+
+
+
+
+
+
